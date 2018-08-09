a3835ac9491da20fbb3b61a06b2ac3d8d44b1534
Joel Osmar Garcia Vazquez <joelgarcia@MacBook-Air-de-Joel.local>
2018-08-09 18:27:15 -0500

    Initial commit

---
+++ lib/joel_time_tool.rb
@@ -0,0 +1,6 @@
+require "joel_time_tool/version"
+require "joel_time_tool/renderer"
+
+module JoelTimeTool
+  # Your code goes here...
+end
---
+++ lib/joel_time_tool/version.rb
@@ -0,0 +1,3 @@
+module JoelTimeTool
+  VERSION = "0.1.0"
+end
